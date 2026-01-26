1100d435e39ee51e136c587506c88ee7d2037d4f
ryantheunissen <contact@ryantheunissen.nl>
2026-01-26 14:10:26 +0100

    Suppresed the UnstableApiUsage warning

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ rootProject.name = "edc-poc"
 pluginManagement {
     repositories { mavenCentral(); gradlePluginPortal() }
 }
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositories { mavenCentral(); mavenLocal() }
 }
